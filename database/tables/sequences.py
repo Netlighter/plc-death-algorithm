d772657e6db6312d52162b7c1907f3f90ee40b6b
GelNiz <angelinanizam@gmail.com>
2022-05-15 00:51:58 +0300

    new endpoints(preview)

--- database/tables/sequences.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import MetaData, Sequence
-
-meta = MetaData()
-
-tool_table_sequence = Sequence('tool_id_seq', metadata=meta)
-sensor_table_sequence = Sequence('sensor_id_seq', metadata=meta)
-accident_type_table_sequence = Sequence('accident_type_id_seq', metadata=meta)
-accident_table_sequence = Sequence('accident_id_seq',metadata=meta)
-sensor_value_table_sequence = Sequence('sensor_value_id_seq', metadata=meta)
